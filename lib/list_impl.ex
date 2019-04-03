faded728d9a8001447057c6d34b81bb800be24b9
Adam Lancaster <lancaster28@hotmail.com>
2019-04-03 22:28:30 +0100

    reorganize rebuild, rejoice!

---
+++ lib/list_impl.ex
@@ -0,0 +1,89 @@
+defimpl Zip, for: List do
+  @doc """
+  Adds each element in the two lists pairwise, meaning, the first element in a
+  and the first element in b are added together. Then the second element in each
+  are and so on. Returns a list of the result of those additions
+  """
+  def add(a, b), do: elementwise(a, b, Kernel, :+)
+
+  @doc """
+  Multiplies each element in the two lists pairwise, meaning, the first element in a
+  and the first element in b are multiplied together. Then the second element in each
+  are and so on. Returns a list of the result of those multiplications
+  """
+  def multiply(a, b), do: elementwise(a, b, Kernel, :*)
+
+  @doc """
+  Subtracts each element in the two lists pairwise, meaning, the first element in a
+  and the first element in b are subtracted together. Then the second element in each
+  are and so on. Returns a list of the result of those subtractions
+  """
+  def subtract(a, b), do: elementwise(a, b, Kernel, :-)
+
+  @doc """
+  Divides each element in the two lists pairwise, meaning, the first element in a
+  and the first element in b are divided. Then the second element in each
+  are and so on. Returns a list of the result of those divisions
+  """
+  def divide(a, b), do: elementwise(a, b, Kernel, :/)
+
+  @doc """
+  Calls the given fun with each element in the two lists pairwise, meaning, fun is first called with
+  the first element in a and the first element in b. Then it is called with the second element in each
+  list, then the third and so on.
+  """
+  def apply(a, b, fun), do: elementwise(a, b, fun)
+
+  @doc """
+  Calls the given mod / fun with each element in the two lists pairwise, meaning, the mod / fun is
+  first called with the first element in a and the first element in b. Then it is called with the
+  second element in each list, then the third and so on.
+  """
+  def apply(a, b, mod, fun), do: elementwise(a, b, mod, fun)
+
+  @doc """
+  Calls the given mod / fun with each element in the two lists pairwise, meaning, the mod / fun is
+  first called with the first element in a as the first argument to the mod / fun, and the first element
+  in b as the second argument to the mod / fun. Then it is called with the second element in each list
+  as arguments 1 and 2, and so on down the lists.
+
+  The extra args are appended to the end of the argument list.
+  """
+  def apply(a, b, mod, fun, args), do: elementwise(a, b, mod, fun, args)
+
+  def elementwise(a, b, fun) do
+    if length(a) !== length(b) do
+      raise(Zip.CollectionsOfDifferentSizes, {a, b})
+    end
+
+    a
+    |> Enum.with_index()
+    |> Enum.map(fn {item, index} ->
+      fun.(item, Enum.at(b, index))
+    end)
+  end
+
+  def elementwise(a, b, mod, fun, args) do
+    if length(a) !== length(b) do
+      raise(Zip.CollectionsOfDifferentSizes, {a, b})
+    end
+
+    a
+    |> Enum.with_index()
+    |> Enum.map(fn {item, index} ->
+      Kernel.apply(mod, fun, [item, Enum.at(b, index)] ++ args)
+    end)
+  end
+
+  def elementwise(a, b, mod, fun) do
+    if length(a) !== length(b) do
+      raise(Zip.CollectionsOfDifferentSizes, {a, b})
+    end
+
+    a
+    |> Enum.with_index()
+    |> Enum.map(fn {item, index} ->
+      Kernel.apply(mod, fun, [item, Enum.at(b, index)])
+    end)
+  end
+end
---
+++ test/list_impl_test.exs
@@ -0,0 +1,79 @@
+defmodule ListImplTest do
+  use ExUnit.Case
+  doctest Zip
+
+  describe "add/2" do
+    test "adds each element in the list elementwise" do
+      assert Zip.add([2, 4, 6], [2, 4, 6]) == [4, 8, 12]
+    end
+
+    test "empty lists" do
+      assert Zip.add([], []) == []
+    end
+
+    test "differing list lengths" do
+      assert_raise Zip.CollectionsOfDifferentSizes, fn -> Zip.add([1, 2], [1]) end
+    end
+  end
+
+  describe "subtract/2" do
+    test "subtracts each element in the list elementwise" do
+      assert Zip.subtract([2, 4, 6], [2, 4, 6]) == [0, 0, 0]
+    end
+
+    test "empty lists" do
+      assert Zip.subtract([], []) == []
+    end
+
+    test "differing list lengths" do
+      assert_raise Zip.CollectionsOfDifferentSizes, fn -> Zip.subtract([1, 2], [1]) end
+    end
+  end
+
+  describe "multiply/2" do
+    test "multiplies each element in the list elementwise" do
+      assert Zip.multiply([2, 4, 6], [2, 4, 6]) == [4, 16, 36]
+    end
+
+    test "empty lists" do
+      assert Zip.multiply([], []) == []
+    end
+
+    test "differing list lengths" do
+      assert_raise Zip.CollectionsOfDifferentSizes, fn -> Zip.multiply([1, 2], [1]) end
+    end
+  end
+
+  describe "divide/2" do
+    test "divides each element in the list elementwise" do
+      assert Zip.divide([2, 8, 6], [2, 2, 6]) == [1.0, 4.0, 1.0]
+    end
+
+    test "empty lists" do
+      assert Zip.divide([], []) == []
+    end
+
+    test "differing list lengths" do
+      assert_raise Zip.CollectionsOfDifferentSizes, fn -> Zip.divide([1, 2], [1]) end
+    end
+  end
+
+  describe "apply/3" do
+    test "Applies the given fun elementwise to the things in the list" do
+      assert Zip.apply(["TEST"], ["ING"], fn a, b -> a <> b end) == ["TESTING"]
+    end
+  end
+
+  describe "apply/4" do
+    test "Applies the given mod / fun elementwise to the things in the list" do
+      assert Zip.apply(["TEST ING"], ["ING"], String, :bag_distance) == [0.375]
+    end
+  end
+
+  describe "apply/5" do
+    test "Applies the given mod / fun args elementwise to the things in the list" do
+      assert Zip.apply([[1, 2, 3, 4], [5, 6, 7, 8]], [3, 2], Enum, :chunk_every, [3, :discard]) ==
+               [[[1, 2, 3]], [[5, 6]]]
+    end
+  end
+end

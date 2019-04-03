faded72df376ec2dd28980cb905aa2aae60e0e6a
Adam Lancaster <lancaster28@hotmail.com>
2019-04-03 22:10:41 +0100

    Implement apply/3
    this allows a two airity anonymous function as a last arg to Zip.apply

--- lib/zip.ex
+++ lib/zip.ex
@@ -1,19 +1,54 @@
 defprotocol Zip do
+  @moduledoc """
+  The Zip interface allows all sorts of operations on collections. If you squint you might see an
+  applicative.
+
+  The collections need to have some sort of order for this interface to return stable results, so
+  a good example is a list. The zip interface allows us, for example, to add elements from to lists
+  together pairwise. What does pairwise mean? You take the first element in the first list, and the
+  first element in the second list and add them together. Then the second elements in each list get
+  added together and so on.
+
+  ### Example
+
+      iex> Zip.add([1, 2], [1, 2])
+      [2, 4]
+
+  """
+
   def add(a, b)
   def multiply(a, b)
   def subtract(a, b)
   def divide(a, b)
+  def apply(a, b, fun)
   def apply(a, b, mod, fun)
   def apply(a, b, mod, fun, args)
 end
 
 defimpl Zip, for: List do
+  @doc """
+  Adds each element in the two lists pairwise, meaning, the first element in a
+  and the first element in b are added together. Then the second element
+  """
   def add(a, b), do: elementwise(a, b, Kernel, :+)
   def multiply(a, b), do: elementwise(a, b, Kernel, :*)
   def subtract(a, b), do: elementwise(a, b, Kernel, :-)
   def divide(a, b), do: elementwise(a, b, Kernel, :/)
+  def apply(a, b, fun), do: elementwise(a, b, fun)
   def apply(a, b, mod, fun), do: elementwise(a, b, mod, fun)
   def apply(a, b, mod, fun, args), do: elementwise(a, b, mod, fun, args)
+
+  def elementwise(a, b, fun) do
+    if length(a) !== length(b) do
+      raise(Zip.CollectionsOfDifferentSizes, {a, b})
+    end
+
+    a
+    |> Enum.with_index()
+    |> Enum.map(fn {item, index} ->
+      fun.(item, Enum.at(b, index))
+    end)
+  end
 
   def elementwise(a, b, mod, fun, args) do
     if length(a) !== length(b) do
@@ -23,7 +58,7 @@ defimpl Zip, for: List do
     a
     |> Enum.with_index()
     |> Enum.map(fn {item, index} ->
-      apply(mod, fun, [item, Enum.at(b, index)] ++ args)
+      Kernel.apply(mod, fun, [item, Enum.at(b, index)] ++ args)
     end)
   end
 
@@ -35,7 +70,7 @@ defimpl Zip, for: List do
     a
     |> Enum.with_index()
     |> Enum.map(fn {item, index} ->
-      apply(mod, fun, [item, Enum.at(b, index)])
+      Kernel.apply(mod, fun, [item, Enum.at(b, index)])
     end)
   end
 end
--- test/zip_test.exs
+++ test/zip_test.exs
@@ -60,11 +60,17 @@ defmodule ZipTest do
 
   describe "apply/3 - List" do
     test "Applies the given mod / fun elementwise to the things in the list" do
-      assert Zip.apply(["TEST ING"], ["ING"], String, :bag_distance) == [0.375]
+      assert Zip.apply(["TEST"], ["ING"], fn a, b -> a <> b end) == ["TESTING"]
     end
   end
 
   describe "apply/4 - List" do
+    test "Applies the given mod / fun elementwise to the things in the list" do
+      assert Zip.apply(["TEST ING"], ["ING"], String, :bag_distance) == [0.375]
+    end
+  end
+
+  describe "apply/5 - List" do
     test "Applies the given mod / fun args elementwise to the things in the list" do
       assert Zip.apply([[1, 2, 3, 4], [5, 6, 7, 8]], [3, 2], Enum, :chunk_every, [3, :discard]) ==
                [[[1, 2, 3]], [[5, 6]]]

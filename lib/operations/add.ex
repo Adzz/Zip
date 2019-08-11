fadedcfe9e00faff25323e0b2aa8d9d8371fcb76
Adz <email@test.example>
2019-08-11 12:40:11 +0100

    more faff
    Experiments with separating the Zip.Add and Add - the difference being
    one requires 0 arguments at the time of creation of the operation, and
    the other requires one argument at the time of creation:
    
    %Zip.Add{} vs %Add{value: 10}
    
    We should think of better names

--- lib/operations/add.ex
+++ lib/operations/add.ex
@@ -1,45 +1,96 @@
 defprotocol Add do
-  # I think if you need free variables, that is where the value comes in.
-  # For example, to Add, you need two things to add together. If you Zip.add
-  # the the two variables come from the lists. If you map, the other one has to
-  # come from somewhere else.
-  defstruct [:value, zip: &__MODULE__.zip/2, mapm: &__MODULE__.mapm/2]
-  def zip(a, b)
+  @moduledoc """
+  Use this Add operation when the action you are doing will provide one variable to the add function.
+  You can then provide the other under the value key. An example is map'ing over a list. When maping
+  over a list you take each element out of the list and pass that to the adding function. But in
+  order to 'add' you need two things to add together. So in this case you would provide the second
+  through this adding function:
+
+  MapM.apply([1,2,3,4,5], %Add{value: 10})
+
+  This is in contrast to a Zipping action, which would take the two variables to be added together
+  from each of the lists it is adding together.
+
+  Zip.apply([1], [1], %Zip.Add{})
+  """
+  @enforce_keys [:value]
+  defstruct [:value, mapm: &__MODULE__.mapm/2]
   def mapm(x, operation)
 end
 
+# Maybe a better name is one that indicates it has no free variables, so that it is generalisable
+# to any action that will only provide both, not just to zip? What other actions provide both args?
+# something something partial application?
+defprotocol Zip.Add do
+  @moduledoc """
+  Use this Add operation when the action you are doing will provide both variables to the add function.
+  An example is zipping two lists together:
+
+  Zip.apply([1], [1], %Zip.Add{})
+
+  This is in contrast to a Mapping action, which would take only one variable from the list being
+  mapped over, and would therefore require another variable, which could be provided from the Add
+  operation itself.
+
+  MapM.apply([1,2,3,4,5], %Add{value: 10})
+  """
+  defstruct zip: &__MODULE__.zip/2
+  def zip(a, b)
+end
+
+defmodule Int do
+  def add(a, b) when is_integer(b), do: a + b
+end
+
 defimpl Add, for: Integer do
-  def zip(a, b) when is_integer(b), do: a + b
-  def mapm(x, %{value: value}) when is_integer(value), do: x + value
+  def mapm(x, %{value: value}) when is_integer(value), do: Int.add(x, value)
   def mapm(x, %{value: value = %Decimal{}}), do: Decimal.to_integer(Decimal.round(value)) + x
 end
 
+defimpl Zip.Add, for: Integer do
+  def zip(a, b) when is_integer(b), do: Int.add(a, b)
+end
+
 defimpl Add, for: Decimal do
-  def zip(decimal, decimal_2 = %Decimal{}), do: Decimal.add(decimal, decimal_2)
   def mapm(x, %{value: value = %Decimal{}}), do: Decimal.add(x, value)
   def mapm(x, %{value: value}) when is_integer(value), do: Decimal.add(x, Decimal.new(value))
 end
 
+defimpl Zip.Add, for: Decimal do
+  def zip(decimal, decimal_2 = %Decimal{}), do: Decimal.add(decimal, decimal_2)
+end
+
 defimpl Add, for: List do
-  def zip(list_1, list_2) when is_list(list_2), do: Zip.apply(list_1, list_2, %Add{})
   def mapm(list, operation), do: MapM.apply(list, operation)
 end
 
+defimpl Zip.Add, for: List do
+  def zip(list_1, list_2) when is_list(list_2), do: Zip.apply(list_1, list_2, %Zip.Add{})
+end
+
 defimpl Add, for: Map do
-  def zip(map, map_2) when is_map(map_2), do: Zip.apply(map, map_2, %Add{})
   def mapm(map, operation), do: MapM.apply(map, operation)
 end
 
+defimpl Zip.Add, for: Map do
+  def zip(map, map_2) when is_map(map_2), do: Zip.apply(map, map_2, %Zip.Add{})
+end
+
 defimpl Add, for: PID do
-  def zip(pid, pid_2) when is_pid(pid_2), do: Zip.apply(pid, pid_2, %Add{})
   def mapm(pid, operation), do: MapM.apply(pid, operation)
+end
+
+defimpl Zip.Add, for: PID do
+  def zip(pid, pid_2) when is_pid(pid_2), do: Zip.apply(pid, pid_2, %Zip.Add{})
 end
 
 # Streams
 defimpl Add, for: Function do
-  def zip(stream, stream_2) when is_function(stream_2) do
-    Zip.apply(stream, stream_2, %Add{})
-  end
-
   def mapm(stream, operation), do: MapM.apply(stream, operation)
+end
+
+defimpl Zip.Add, for: Function do
+  def zip(stream, stream_2) when is_function(stream_2) do
+    Zip.apply(stream, stream_2, %Zip.Add{})
+  end
 end
--- lib/zip.ex
+++ lib/zip.ex
@@ -42,11 +42,6 @@ defprotocol MapM do
 end
 
 defimpl MapM, for: List do
-  # Naming the key the same as the action is better for
-  # operations that span different actions. So, Add (operation)
-  # can be used for Zip and MapM (i dont have a word for these higher order
-  # actions), meaning we can't just use calculate, we should use
-  # %{zip: zip} for Zip and %{mapm: mapm} for MapM
   def apply(a, operation = %{mapm: mapm}) do
     Enum.map(a, fn x -> mapm.(x, operation) end)
   end
@@ -55,11 +50,6 @@ defimpl MapM, for: List do
     Enum.map(a, fun)
   end
 end
-
-# What are the semantics for filter? Realistically you need to filter based on a
-# condition, meaning the semantics are a function that returns a boolean.
-# Difficulty is that can be with another variable or not, but that can be
-# encapsulated by the operation:
 
 defprotocol Filter do
   def apply(x, operation)
@@ -73,7 +63,36 @@ defimpl Filter, for: List do
   # to compare.
   # The only way to tell is if the operation has a value key. This key could
   # become the way that an operation adds any free variables (e.g. if it needs
-  # two free variables, values becomes an array)
+  # two free variables, values becomes an array).
+
+  # But what about for operations that span actions,that can sometimes have a free
+  # variable sometimes not...
+  # For example, Add, when zipping gets two variables from the thing being zipped.
+  # But when maping you need a free variable. In that case the pattern is to have
+  # a non null thing for value? But what if nil is a legit value? Should we use a list
+  # then? Or will he have to cater for list when 2 or more extra args needed.
+  # not a list all the other time?
+  # What if someone puts a value there? Then do we include it in to the operation?
+  # Will it always make sense to do so? Or are Zip.add and Map.add just different
+  # types of things?
+
+  # They are in that the semantics of where they get the variables from is way different...
+  # for one case (map) it makes sense that there will always be one value in the operation.
+  # For zip it doesn't.
+
+  # so can we abstract only what is similar, re-use the same operations but create different
+  # operations?
+  # Zip.apply([1], [2], %Zip.Add{})
+  # MapM.apply([1], [2], %Add{})
+  # The downside here is the user has to know which "Add" to use. Explosion of code also, as there
+  # are now N many "Add" protocols that we may have to implement... But 0 chance of a certain class
+  # of error... which class? The passing the wrong kind of operation to an Action error
+  # This becomes impossible:
+  # Zip.apply([1], [2], %Add{value: 10})
+  # As does this:
+  # MapM.apply([1], [2], %Zip.Add{})
+  # And we can even then enforce the value when we know we need it so this is not possible:
+  # MapM.apply([1], %Add{})
   def apply(a, operation = %{filter: filter, value: _}) do
     Enum.filter(a, fn x -> filter.(x, operation) end)
   end
